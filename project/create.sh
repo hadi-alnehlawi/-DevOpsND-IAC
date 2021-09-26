4e015fad556d53778dba4f326732f12c41f6af8b
hadi <hadi.alnehlawi@gmail.com>
2021-09-26 19:01:30 -0300

    compelte

--- project/create.sh
+++ project/create.sh
@@ -1,2 +1,2 @@
-aws cloudformation create-stack --capabilities CAPABILITY_NAMED_IAM  --stack-name $1 --template-body file://$2  --parameters file://$3 --region=us-west-2
+uaws cloudformation create-stack --capabilities CAPABILITY_NAMED_IAM  --stack-name $1 --template-body file://$2  --parameters file://$3 --region=us-east-1
 
